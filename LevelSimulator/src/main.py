aad7c59568b7e08dcc6ffe7e4026a99f11d9fa78
Kwak <brettkwak@gmail.com>
2025-01-15 03:33:52 +0900

    comment : Add comment

--- LevelSimulator/src/main.py
+++ LevelSimulator/src/main.py
@@ -1,11 +1,14 @@
+# Input Level, EXP
 current_level = input("현재 레벨 : ")
 current_exp = input("현재 경험치 : ")
 level_exp = 0
 
+# Setting Input
 input("일일 퀘스트? (Y/N) : ")
 input("주간 퀘스트? (Y/N) : ")
 input("몬스터 파크 횟수 : ")
 
+# Result Example
 print("계산 전 레벨 / 경험치")
 print("Lv. " + str(current_level))
 print(str(current_exp) + " / " + str(level_exp))
