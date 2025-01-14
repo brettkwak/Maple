b401fbaa683dbfd53d37aa415247eeb8cdf3b44e
Kwak <brettkwak@gmail.com>
2025-01-15 04:06:12 +0900

    feat : Import data using pandas

--- LevelSimulator/src/main.py
+++ LevelSimulator/src/main.py
@@ -1,3 +1,18 @@
+import pandas as pd
+
+# Fuction to load the data
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+# Load all data
+level_exp_data = load_data('../data/Level_EXP.csv')
+daily_quest_data = load_data('../data/Daily_EXP.csv')
+monster_park_data = load_data('../data/Monster_Park.csv')
+# Print
+print(level_exp_data)
+print(daily_quest_data)
+print(monster_park_data)
+
 # Input Level, EXP
 current_level = input("현재 레벨 : ")
 current_exp = input("현재 경험치 : ")
