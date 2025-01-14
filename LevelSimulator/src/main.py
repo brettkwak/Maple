4ac7473d1b3b6b0c68ec846aadb0bfcea217ae38
Kwak <brettkwak@gmail.com>
2025-01-15 03:25:05 +0900

    feat : Add user input

---
+++ LevelSimulator/src/main.py
@@ -0,0 +1,14 @@
+current_level = input("현재 레벨 : ")
+current_exp = input("현재 경험치 : ")
+level_exp = 0
+
+input("일일 퀘스트? (Y/N) : ")
+input("주간 퀘스트? (Y/N) : ")
+input("몬스터 파크 횟수 : ")
+
+print("계산 전 레벨 / 경험치")
+print("Lv. " + str(current_level))
+print(str(current_exp) + " / " + str(level_exp))
+print("계산 후 레벨 / 경험치")
+print("Lv. " + str(current_level))
+print(str(current_exp) + " / " + str(level_exp))
