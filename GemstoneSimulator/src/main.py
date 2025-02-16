e740daa8b186ecc08b0b927bf9cf9d58cc870046
Kwak <brettkwak@gmail.com>
2025-02-17 06:22:35 +0900

    feat : Option for toggling show plot

--- GemstoneSimulator/src/main.py
+++ GemstoneSimulator/src/main.py
@@ -6,6 +6,7 @@ class_name = input("Class Name : ")
 image1_name = input("Core Name : ")
 image2_name = input("Skill Name : ")
 mask_name = input("Mask Name : ")
+show_plot = input("Show Plot? (Yes/No) : ")
 
 def compare_core_with_skill(image1_path, image2_path, mask_path):
     # Read images and mask
@@ -26,28 +27,29 @@ def compare_core_with_skill(image1_path, image2_path, mask_path):
     ssim_score, _ = ssim(gray1, gray2, full=True)
 
     # Show results
-    plt.figure(figsize=(12, 5))
-    plt.subplot(151)
-    plt.imshow(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB))
-    plt.title('Image 1')
+    if show_plot == "Yes":
+        plt.figure(figsize=(12, 5))
+        plt.subplot(151)
+        plt.imshow(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB))
+        plt.title('Image 1')
 
-    plt.subplot(152)
-    plt.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
-    plt.title('Image 2')
+        plt.subplot(152)
+        plt.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
+        plt.title('Image 2')
 
-    plt.subplot(153)
-    plt.imshow(mask, cmap='gray')
-    plt.title('Mask')
+        plt.subplot(153)
+        plt.imshow(mask, cmap='gray')
+        plt.title('Mask')
 
-    plt.subplot(154)
-    plt.imshow(cv2.cvtColor(img1_masked, cv2.COLOR_BGR2RGB))
-    plt.title('Masked Image 1')
+        plt.subplot(154)
+        plt.imshow(cv2.cvtColor(img1_masked, cv2.COLOR_BGR2RGB))
+        plt.title('Masked Image 1')
 
-    plt.subplot(155)
-    plt.imshow(cv2.cvtColor(img2_masked, cv2.COLOR_BGR2RGB))
-    plt.title('Masked Image 2')
+        plt.subplot(155)
+        plt.imshow(cv2.cvtColor(img2_masked, cv2.COLOR_BGR2RGB))
+        plt.title('Masked Image 2')
 
-    plt.show()
+        plt.show()
 
     return {
         'ssim_score': ssim_score,
