49cec0e59d470797e4f4d1b88703c888eac914be
Kwak <brettkwak@gmail.com>
2025-03-09 16:44:48 +0900

    feat : Filter cores using skills
    - Filter cores using skills that were input from user

--- GemstoneSimulator/src/find_combination.py
+++ GemstoneSimulator/src/find_combination.py
@@ -28,7 +28,33 @@ def print_cores(loaded_cores):
     print("=====================\n")
 
 
+# Filter cores using skill input
+def filter_cores_with_skills(cores, target_skills):
+
+    filtered_cores = {}
+
+    for core_num, skills in cores.items():
+        if all(skill in target_skills for skill in skills):
+            filtered_cores[core_num] = skills
+
+    print(f"Found {len(filtered_cores)} cores containing only the specified skills")
+
+    return filtered_cores
+
+
 file_path = f'../data/core_skill_mapping.txt'
 
 cores = load_core_information_from_file(file_path)
-print_cores(cores)
+
+# Input skill names to filter cores
+target_skills = []
+print("Enter skill names : ")
+while True:
+    skill = input()
+    if not skill:
+        break
+    target_skills.append(skill)
+
+filtered_cores = filter_cores_with_skills(cores, target_skills)
+
+print_cores(filtered_cores)
